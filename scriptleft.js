7f193e6d486126e6fe1a3334c4fcea20a6a14bab
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-10-14 05:14:33 -0700

    Create scriptleft.js

---
+++ scriptleft.js
@@ -0,0 +1,177 @@
+var _0x5146 = ['gNbjW', 'body', 'EiFCi', 'ea-smib-forkawesome', 'loaded', 'setItem', 'type', 'onreadystatechange', 'undefined', 'effectiveAppsSocialMediaIconsBar__closed', '.effectiveAppsSocialMediaIconsBar{z-index:\x20100000;right:0%;\x20position:\x20fixed;\x20transition:\x20all\x200.4s;bottom:3%;}.effectiveAppsSocialMediaIconsBarContainer{position:\x20fixed;\x20z-index:\x20100000;bottom:3%;}.effectiveAppsSocialMediaIconsBarController{position:\x20fixed;right:0.2px;\x20z-index:\x201000001;bottom:3%;}.effectiveAppsSocialMediaIconsBar__closed{transition:\x20all\x200.4s;right:\x20-54px;}.effectiveAppsSocialMediaIconsBar\x20a{display:\x20block;text-align:\x20center;display:\x20flex;justify-content:\x20center;align-items:\x20center;height:60px;width:60px;font-size:23.076923076923077px;text-decoration:\x20none;}.effectiveAppsSocialMediaIconsBar\x20a:hover{background-color:\x20#000}.ea-smib-facebook{background:\x20#4267b2;color:\x20#fff}.ea-smib-instagram{color:\x20#fff;background:\x20#d6249f;background:\x20radial-gradient(circle\x20at\x2030%\x20107%,\x20#fdf497\x200,\x20#fdf497\x205%,\x20#fd5949\x2045%,\x20#d6249f\x2060%,\x20#285aeb\x2095%)}.ea-smib-instagram:hover{background-color:\x20#000;background:\x20#000}.ea-smib-youtube{background:\x20red;color:\x20#fff}.ea-smib-linkedin{background:\x20#007bb5;color:\x20#fff}.ea-smib-twitter{background:\x20#55acee;color:\x20#fff}.ea-smib-pinterest{background:\x20#bd081c;color:\x20#fff}.ea-smib-tumblr{background:\x20#34465d;color:\x20#fff}.ea-smib-snapchat{background:\x20#fffc00;color:\x20black;}.ea-smib-snapchat:hover{color:\x20#fff}.ea-smib-behance{background:\x20#053eff;color:\x20#fff}.ea-smib-vimeo{background:\x20#1ab7ea;color:\x20#fff}.ea-smib-wechat{background:\x20#7BB32E;color:\x20#fff}.ea-smib-messenger{background:\x20#0084FF;color:\x20#fff}.ea-smib-whatsapp{background:\x20#4AC959;color:\x20#fff}.ea-smib-telegram{background:\x20#0088cc;color:\x20#fff}.ea-smib-email{background:\x20#5cbdea;color:\x20#fff}.ea-smib-tiktok{background:\x20#000000;color:\x20#fff}.ea-smib-tiktok:hover{fill:\x20#fff}', 'getItem', 'getElementsByTagName', 'meta', 'https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css', 'home', 'SMIB_SCRIPT_INJECTED', 'style', 'innerHTML', 'DPFbH', 'contains', 'createElement', '#eaSmibBarController', 'RvWoL', 'pageType', 'onmouseover', 'opened', 'onload', '<div\x20class=\x22effectiveAppsSocialMediaIconsBarContainer\x22\x20id=\x22eaSmibContainer\x22\x20onmouseleave=\x22_0x289045()\x22\x20onmouseover=\x22_0xef4bf()\x22><div\x20id=\x22eaSmibBarController\x22\x20class=\x22effectiveAppsSocialMediaIconsBarController\x22\x20style=\x22display:\x20none\x22><i\x20onclick=\x22_0x2d1839()\x22\x20style=\x22color:white;\x22\x20class=\x22fa\x20fa-close\x22\x20style=\x22color:\x20white;\x22></i></div><div\x20id=\x22eaSmib\x22\x20class=\x22effectiveAppsSocialMediaIconsBar\x20effectiveAppsSocialMediaIconsBar__closed\x22><a onmouseover="this.style.color=\'white\'" href="https://facebook.com"  target="_blank" class="ea-smib-twitter"><i class="fa fa-twitter"></i></a><a onmouseover="this.style.color=\'white\'" href="https://facebook.com"  target="_blank" class="ea-smib-linkedin"><i class="fa fa-linkedin"></i></a></div></div>', 'media', 'ea-smib-fontawesome', 'link', 'eaSmib', 'collapsed', 'appendChild', 'effectiveAppsSmibState', 'href', 'classList', 'text/css', 'dlLPt', 'eaSmibContainer', 'complete', 'head', 'readyState', 'pwtgi', '<div\x20class=\x22effectiveAppsSocialMediaIconsBarContainer\x22\x20id=\x22eaSmibContainer\x22\x20onmouseleave=\x22_0x289045()\x22\x20onmouseover=\x22_0xef4bf()\x22><div\x20id=\x22eaSmibBarController\x22\x20class=\x22effectiveAppsSocialMediaIconsBarController\x22\x20style=\x22display:\x20none\x22><i\x20onclick=\x22_0x2d1839()\x22\x20style=\x22color:white;\x22\x20class=\x22fa\x20fa-close\x22\x20style=\x22color:\x20white;\x22></i></div><div\x20id=\x22eaSmib\x22\x20class=\x22effectiveAppsSocialMediaIconsBar\x22><a onmouseover="this.style.color=\'white\'" href="https://facebook.com"  target="_blank" class="ea-smib-twitter"><i class="fa fa-twitter"></i></a><a onmouseover="this.style.color=\'white\'" href="https://facebook.com"  target="_blank" class="ea-smib-linkedin"><i class="fa fa-linkedin"></i></a></div></div>', 'all', 'src', 'getElementById', 'removeChild', 'rel', 'BwXNH', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', 'script', 'stylesheet', 'prepend', 'fadeOut', 'toggle'];
+(function (_0x47d1ea, _0x5146ca) {
+	var _0x53a129 = function (_0x1ea887) {
+		while (--_0x1ea887) {
+			_0x47d1ea['push'](_0x47d1ea['shift']());
+		}
+	};
+	_0x53a129(++_0x5146ca);
+}(_0x5146, 0x191));
+var _0x53a1 = function (_0x47d1ea, _0x5146ca) {
+	_0x47d1ea = _0x47d1ea - 0x0;
+	var _0x53a129 = _0x5146[_0x47d1ea];
+	return _0x53a129;
+};
+
+function _0x3aa448(_0x45cda7, _0xaec9f7) {
+	var _0x542cb0 = document[_0x53a1('0x1a')](_0x53a1('0x0'));
+	_0x542cb0[_0x53a1('0x34')] = _0x45cda7;
+	var _0x28de0c = document['getElementsByTagName']('head')[0x0],
+		_0x3d7726 = ![];
+	_0x28de0c[_0x53a1('0x27')](_0x542cb0);
+	_0x542cb0[_0x53a1('0x20')] = _0x542cb0[_0x53a1('0xc')] = function () {
+		if ('vPcbK' === _0x53a1('0x1c')) {
+			var _0x517442 = document[_0x53a1('0x1a')](_0x53a1('0x0'));
+			_0x517442[_0x53a1('0x34')] = _0x45cda7;
+			var _0x2b828c = document['getElementsByTagName'](_0x53a1('0x2f'))[0x0],
+				_0x418875 = ![];
+			_0x2b828c[_0x53a1('0x27')](_0x517442);
+			_0x517442[_0x53a1('0x20')] = _0x517442[_0x53a1('0xc')] = function () {
+				if (!_0x418875 && (!this[_0x53a1('0x30')] || this[_0x53a1('0x30')] == _0x53a1('0x9') || this[_0x53a1('0x30')] == _0x53a1('0x2e'))) {
+					_0x418875 = !![];
+					_0xaec9f7();
+					_0x517442['onload'] = _0x517442[_0x53a1('0xc')] = null;
+					_0x2b828c['removeChild'](_0x517442);
+				}
+			};
+		} else {
+			if (!_0x3d7726 && (!this[_0x53a1('0x30')] || this[_0x53a1('0x30')] == _0x53a1('0x9') || this[_0x53a1('0x30')] == 'complete')) {
+				_0x3d7726 = !![];
+				_0xaec9f7();
+				_0x542cb0[_0x53a1('0x20')] = _0x542cb0[_0x53a1('0xc')] = null;
+				_0x28de0c[_0x53a1('0x36')](_0x542cb0);
+			}
+		}
+	};
+}
+
+function _0x107898(_0x5c3aa0) {
+	var _0x53d30d = document[_0x53a1('0x11')](_0x53a1('0x2f'))[0x0],
+		_0x54b779 = ![];;
+	var _0x2a3d50 = document['createElement'](_0x53a1('0x24'));
+	_0x2a3d50['id'] = _0x53a1('0x8');
+	_0x2a3d50['rel'] = _0x53a1('0x1');
+	_0x2a3d50[_0x53a1('0xb')] = _0x53a1('0x2b');
+	_0x2a3d50[_0x53a1('0x29')] = _0x53a1('0x13');
+	_0x2a3d50[_0x53a1('0x22')] = _0x53a1('0x33');
+	_0x53d30d['appendChild'](_0x2a3d50);
+	_0x2a3d50['onload'] = _0x2a3d50[_0x53a1('0xc')] = function () {
+		if (!_0x54b779 && (!this[_0x53a1('0x30')] || this['readyState'] == _0x53a1('0x9') || this[_0x53a1('0x30')] == _0x53a1('0x2e'))) {
+			if (_0x53a1('0x31') === _0x53a1('0x5')) {
+				jQuery(_0x53a1('0x6'))[_0x53a1('0x2')](_0x53a1('0x21'));
+			} else {
+				_0x54b779 = !![];
+				_0x5c3aa0();
+			}
+		}
+	};
+}
+
+function _0x13b144(_0x3b8a4b, _0x117b86) {
+	var _0x395edd = _0x53a1('0xf');
+	var _0x161c34 = document['head'] || document[_0x53a1('0x11')](_0x53a1('0x2f'))[0x0],
+		_0x5b21c4 = document[_0x53a1('0x1a')](_0x53a1('0x16'));
+	document[_0x53a1('0x2f')]['appendChild'](_0x5b21c4);
+	_0x5b21c4[_0x53a1('0x17')] = _0x395edd;
+	var _0x161c34 = document['getElementsByTagName'](_0x53a1('0x2f'))[0x0],
+		_0x70d76f = ![];;
+	var _0x2845f8 = document['createElement'](_0x53a1('0x24'));
+	_0x2845f8['id'] = _0x53a1('0x23');
+	_0x2845f8[_0x53a1('0x37')] = _0x53a1('0x1');
+	_0x2845f8[_0x53a1('0xb')] = _0x53a1('0x2b');
+	_0x2845f8[_0x53a1('0x29')] = _0x3b8a4b;
+	_0x2845f8['media'] = _0x53a1('0x33');
+	_0x161c34[_0x53a1('0x27')](_0x2845f8);
+	_0x2845f8[_0x53a1('0x20')] = _0x2845f8[_0x53a1('0xc')] = function () {
+		if ('fbBmA' !== 'mVhFq') {
+			if (!_0x70d76f && (!this[_0x53a1('0x30')] || this[_0x53a1('0x30')] == 'loaded' || this[_0x53a1('0x30')] == _0x53a1('0x2e'))) {
+				_0x70d76f = !![];
+				_0x117b86();
+			}
+		} else {
+			localStorage[_0x53a1('0xa')](_0x53a1('0x28'), _0x53a1('0x26'));
+			var _0x4c118b = document[_0x53a1('0x35')](_0x53a1('0x2d'))[_0x53a1('0x1e')];
+			document[_0x53a1('0x35')](_0x53a1('0x2d'))[_0x53a1('0x1e')] = null;
+			document[_0x53a1('0x35')](_0x53a1('0x25'))[_0x53a1('0x2a')]['toggle'](_0x53a1('0xe'), !![]);
+			jQuery(_0x53a1('0x1b'))[_0x53a1('0x3')]();
+			setTimeout(function () {
+				document['getElementById'](_0x53a1('0x2d'))[_0x53a1('0x1e')] = _0x4c118b;
+			}, 0x5dc);
+		}
+	};
+}
+
+function _0x59514e() {
+	if (window[_0x53a1('0x15')] == undefined) {
+		if (_0x53a1('0x38') === 'xcrPe') {
+			return;
+		} else {
+			window[_0x53a1('0x15')] = !![];
+			if (0x2 === 1) {
+				if (ShopifyAnalytics[_0x53a1('0x12')]['page'][_0x53a1('0x1d')] !== _0x53a1('0x14')) {
+					if (_0x53a1('0x7') === 'oCGsl') {
+						_0x13b144(_0x53a1('0x39'), function () {
+							_0x107898(_0x59514e);
+						});
+					} else {
+						return;
+					}
+				}
+			}
+			if (localStorage[_0x53a1('0x10')](_0x53a1('0x28')) === null || localStorage[_0x53a1('0x10')](_0x53a1('0x28')) === _0x53a1('0x1f')) {
+				jQuery('body')[_0x53a1('0x2')](_0x53a1('0x32'));
+			} else {
+				jQuery(_0x53a1('0x6'))[_0x53a1('0x2')](_0x53a1('0x21'));
+			}
+		}
+	}
+}
+
+function _0x289045() {
+	if (!document[_0x53a1('0x35')]('eaSmib')[_0x53a1('0x2a')][_0x53a1('0x19')](_0x53a1('0xe'))) {
+		jQuery('#eaSmibBarController')[_0x53a1('0x3')]();
+	}
+}
+
+function _0xef4bf() {
+	if (document[_0x53a1('0x35')](_0x53a1('0x25'))[_0x53a1('0x2a')]['contains'](_0x53a1('0xe'))) {
+		if (_0x53a1('0x18') === _0x53a1('0x18')) {
+			document[_0x53a1('0x35')](_0x53a1('0x25'))[_0x53a1('0x2a')][_0x53a1('0x4')](_0x53a1('0xe'), ![]);
+			localStorage[_0x53a1('0xa')](_0x53a1('0x28'), _0x53a1('0x1f'));
+		} else {
+			document[_0x53a1('0x35')]('eaSmib')[_0x53a1('0x2a')][_0x53a1('0x4')]('effectiveAppsSocialMediaIconsBar__closed', ![]);
+			localStorage['setItem'](_0x53a1('0x28'), _0x53a1('0x1f'));
+		}
+	} else {
+		jQuery(_0x53a1('0x1b'))['fadeIn']();
+	}
+}
+
+function _0x2d1839() {
+	localStorage[_0x53a1('0xa')]('effectiveAppsSmibState', _0x53a1('0x26'));
+	var _0x19b0d1 = document[_0x53a1('0x35')](_0x53a1('0x2d'))[_0x53a1('0x1e')];
+	document[_0x53a1('0x35')](_0x53a1('0x2d'))[_0x53a1('0x1e')] = null;
+	document[_0x53a1('0x35')](_0x53a1('0x25'))[_0x53a1('0x2a')][_0x53a1('0x4')]('effectiveAppsSocialMediaIconsBar__closed', !![]);
+	jQuery(_0x53a1('0x1b'))[_0x53a1('0x3')]();
+	setTimeout(function () {
+		document[_0x53a1('0x35')](_0x53a1('0x2d'))['onmouseover'] = _0x19b0d1;
+	}, 0x5dc);
+}
+if (typeof jQuery == _0x53a1('0xd')) {
+	_0x3aa448('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', function () {
+		_0x13b144(_0x53a1('0x39'), function () {
+			if (_0x53a1('0x2c') !== 'ogpKl') {
+				_0x107898(_0x59514e);
+			} else {
+				done = !![];
+				success();
+				script[_0x53a1('0x20')] = script[_0x53a1('0xc')] = null;
+				head[_0x53a1('0x36')](script);
+			}
+		});
+	});
+} else {
+	_0x13b144(_0x53a1('0x39'), function () {
+		_0x107898(_0x59514e);
+	});
+}
