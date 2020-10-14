999b3281d5197e1ea508ffe8a258893dfd6dd60c
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-10-14 06:03:30 -0700

    Create rfunct.js

---
+++ rfunct.js
@@ -0,0 +1,117 @@
+/**/
+__esappsPlatformBoot1602679043653({
+    "status": 1,
+    "data": {
+        "widgets": {
+            "24de4ff7-cc3f-4a9c-80ce-4b3f632588fe": {
+                "status": 1,
+                "data": {
+                    "app": "social-icons",
+                    "settings": {
+                        "widgetId": "9f12d471-00d5-4f5a-a08f-b94f27b31e42",
+                        "widgetOrigin": "shy.elfsight.com",
+                        "items": [{
+                            "type": "facebook",
+                            "url": "https:\/\/www.facebook.com\/elfsight"
+                        }, {
+                            "type": "twitter",
+                            "url": "https:\/\/twitter.com\/elfsight"
+                        }, {
+                            "type": "instagram",
+                            "url": "https:\/\/www.instagram.com\/elfsight\/"
+                        }, {
+                            "type": "youtube",
+                            "url": "https:\/\/www.youtube.com\/elfsight"
+                        }, {
+                            "type": "vimeo",
+                            "url": "https:\/\/vimeo.com\/elfsight"
+                        }, {
+                            "type": "linkedin",
+                            "url": "https:\/\/www.linkedin.com\/company\/elfsight\/"
+                        }, {
+                            "type": "pinterest",
+                            "url": "https:\/\/www.pinterest.com\/elfsight\/"
+                        }, {
+                            "type": "email",
+                            "url": "hello@elfsight.com"
+                        }, {
+                            "url": "",
+                            "type": "",
+                            "iconUrl": ""
+                        }, {
+                            "url": "",
+                            "type": "",
+                            "iconUrl": ""
+                        }],
+                        "size": "40",
+                        "location": "inline",
+                        "position": "center",
+                        "style": "default",
+                        "iconColor": "white",
+                        "bgColor": "native",
+                        "iconColorOnHover": "native",
+                        "bgColorOnHover": "native",
+                        "borderRadius": "circle",
+                        "animation": "bounce",
+                        "transparency": 100,
+                        "transparencyOnHover": 100,
+                        "url": "",
+                        "type": "",
+                        "iconUrl": "",
+                        "deactivate": false,
+                        "showElfsightLogo": false,
+                        "freeLinkUrl": null,
+                        "freeLinkAnchor": null,
+                        "hideElfsightLogo": true
+                    },
+                    "preferences": []
+                }
+            },
+            "f7339559-fae7-4d34-b78a-396a9b1ba7cb": {
+                "status": 1,
+                "data": {
+                    "app": "social-icons",
+                    "settings": {
+                        "widgetId": "469ed8e1-6f2a-4f4b-a8dc-35f3250095b6",
+                        "widgetOrigin": "shy.elfsight.com",
+                        "items": [{
+                            "type": "facebook",
+                            "url": "https:\/\/www.facebook.com\/elfsight"
+                        }, {
+                            "type": "twitter",
+                            "url": "https:\/\/twitter.com\/elfsight"
+                        }, {
+                            "type": "instagram",
+                            "url": "https:\/\/www.instagram.com\/elfsight\/"
+                        }, {
+                            "type": "youtube",
+                            "url": "https:\/\/www.youtube.com\/elfsight"
+                        }],
+                        "size": "32",
+                        "location": "floating",
+                        "position": "left",
+                        "style": "default",
+                        "iconColor": "white",
+                        "bgColor": "native",
+                        "iconColorOnHover": "white",
+                        "bgColorOnHover": "native",
+                        "borderRadius": "square",
+                        "animation": "bounce",
+                        "transparency": 100,
+                        "transparencyOnHover": 100,
+                        "url": "",
+                        "type": "",
+                        "iconUrl": "",
+                        "deactivate": false,
+                        "showElfsightLogo": false,
+                        "freeLinkUrl": null,
+                        "freeLinkAnchor": null,
+                        "hideElfsightLogo": true
+                    },
+                    "preferences": []
+                }
+            }
+        },
+        "assets": ["https:\/\/rawcdn.githack.com\/redaboubkraoui\/cdn\/56a5f2ffd41f07415b848380214455175e824b1e\/scriptDrag.js"]
+    }
+});
