1e0efea585cdde14101c00bde5aaecac31dfda64
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-10-17 00:58:59 -0700

    Create a.js

---
+++ a.js
@@ -0,0 +1,93 @@
+/**/
+__esappsPlatformBoot1602920299882({
+    "status": 1,
+    "data": {
+        "widgets": {
+            "7458f4ef-44f3-442f-92d0-b1f0861e64d2": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "9e8be141-24ac-472b-bf9d-77347d7dd108": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "671c25e4-9b83-48bb-9a3b-630befb05f70": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "cab99b46-ef92-484c-8e41-f8d0aff067b8": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "a9a0f122-7eb0-49ef-b56f-46773eb0a9bf": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "ca15fbaa-523c-4342-84ca-2c13aac171b4": {
+                "status": 1,
+                "data": {
+                    "app": "social-icons",
+                    "settings": {
+                        "widgetId": "ca15fbaa-523c-4342-84ca-2c13aac171b4",
+                        "widgetOrigin": "shy.elfsight.com",
+                        "items": [{
+                            "type": "facebook",
+                            "url": "https:\/\/www.facebook.com\/elfsight"
+                        }, {
+                            "type": "twitter",
+                            "url": "https:\/\/twitter.com\/elfsight"
+                        }, {
+                            "type": "instagram",
+                            "url": "https:\/\/www.instagram.com\/elfsight\/"
+                        }, {
+                            "type": "youtube",
+                            "url": "https:\/\/www.youtube.com\/elfsight"
+                        }, {
+                            "url": "https:\/\/theshopway12.myshopify.com\/admin\/apps\/social-media-icons-app\/panel\/social-icons?hmac=80640efca0d24ac9bd8970ccf0449364af732f4e5e491a5a6c350f4fd24d8e84&locale=en&session=b6eac0298b2bc04c6bc8ee7b75eba796dee128a8e22c366aa2f627ecf378deeb&shop=theshopway12.myshopify.com&timestamp=1602795401",
+                            "type": "apple",
+                            "iconUrl": ""
+                        }, {
+                            "url": "https:\/\/theshopway12.myshopify.com\/admin\/apps\/social-media-icons-app\/panel\/social-icons?hmac=80640efca0d24ac9bd8970ccf0449364af732f4e5e491a5a6c350f4fd24d8e84&locale=en&session=b6eac0298b2bc04c6bc8ee7b75eba796dee128a8e22c366aa2f627ecf378deeb&shop=theshopway12.myshopify.com&timestamp=1602795401",
+                            "type": "amazon",
+                            "iconUrl": ""
+                        }, {
+                            "url": "",
+                            "type": "",
+                            "iconUrl": ""
+                        }],
+                        "size": "48",
+                        "location": "inline",
+                        "position": "left",
+                        "style": "symbol",
+                        "iconColor": "native",
+                        "bgColor": "native",
+                        "iconColorOnHover": "native",
+                        "bgColorOnHover": "native",
+                        "borderRadius": "rounded",
+                        "animation": "fly",
+                        "transparency": 100,
+                        "transparencyOnHover": 100,
+                        "url": "",
+                        "type": "",
+                        "iconUrl": "",
+                        "deactivate": false,
+                        "showElfsightLogo": false,
+                        "freeLinkUrl": null,
+                        "freeLinkAnchor": null,
+                        "hideElfsightLogo": true
+                    },
+                    "preferences": []
+                }
+            },
+            "2426ee85-5cde-4a49-9440-a7d2a8503bfe": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            },
+            "98cce15c-84bf-4205-bca6-c0901da97059": {
+                "status": 0,
+                "reason": "WIDGET_DISABLED"
+            }
+        },
+        "assets": ["https:\/\/shy.elfsight.com\/s\/7c11a672-2d9c-40fb-b216-d0e6b634f000\/elfsight-social-icons-9ac10218854e\/app\/dist\/social-icons.js"]
+    }
+});
