9beb92f57fc272b2bd0a1888026ddbb7b346f89f
younes rahim <30657746+younes16@users.noreply.github.com>
2020-11-04 02:58:56 +0100

    Update chunk.js

--- chunk.js
+++ chunk.js
@@ -192,7 +192,7 @@ if(path ==='')
    var shop =window.location.host
    $.ajax({
        type: "POST",
-        url:"https://cc4317b260ad.ngrok.io/setposition",
+        url:"https://747f1a994b55.ngrok.io/setposition",
      data:{ 'position' : posistion ,'path' : path,'statu': 1,'shop': shop  },
         success: function(data) {
         if (data) {
